5856253e6d287dd42fb76e3ac24ff2aa2a1e3c9d
Rodrigo Faria <jrfaria@ua.pt>
2024-02-22 12:37:16 +0000

    coaches

--- assignment1/Coach.java
+++ assignment1/Coach.java
@@ -1,13 +1,25 @@
 package assignment1;
 
+import java.lang.Math;
+
 public class Coach {
     private Player[] team;
-
+    private boolean method;
+    private int selected = 0;
+    
     public Coach(Player[] team){
         this.team = team;
+        this.method = Math.random() < 0.5;
     }
 
     public Player[] selectPlayers(){
+        if (method)
+            return selectPlayersSweaty();
+        else
+            return selectPlayersLazy();
+    }
+
+    public Player[] selectPlayersSweaty(){
         Player[] sorted = new Player[team.length];
 
         for (int i = 0; i < team.length; i++){
@@ -25,5 +37,14 @@ public class Coach {
         }
 
         return new Player[]{sorted[0], sorted[1], sorted[2]};
+    }
+
+    public Player[] selectPlayersLazy(){
+        selected %= team.length;
+        return new Player[]{
+            team[selected++ % team.length],
+            team[selected++ % team.length],
+            team[selected++ % team.length]
+        };
     }
 }
