57a89cd3cb61d7000ce9542d0edee368edb1edc7
Rodrigo Faria <jrfaria@ua.pt>
2024-05-02 18:04:12 +0100

    inb4 java doc

--- assignment2/commInfra/MessageType.java
+++ assignment2/commInfra/MessageType.java
@@ -1,49 +1,160 @@
 package commInfra;
 
+/**
+ * MessageType is an enumeration of all the message types that can be exchanged between the entities.
+ */
 public enum MessageType { // TODO
-    // GeneralRepos
-    SETNFIC, // Set Log File Name 
-    NFICDONE, // Log File Name Done
-    STREFST, // Start Referee State
-    STCOAST, // Start Coach State
-    STCONTST, // Start Contestant State
-    STCONTSTR, // Start Contestant Strength
-    ADDCONT, // Add Contestant
-    RMCONT, // Remove Contestant
-    STRP, // Set Rope Position
-    STG, // Start Game
-    EOG, // End Of Game
-    EOM, // End Of Match
+    // GeneralRepos    
+    /**
+     * SETNFIC - Set Log File Name
+     */
+    SETNFIC,
+    /**
+     * NFICDONE - Log File Name Done
+     */
+    NFICDONE,
+    /*
+     * STREFST - Start Referee State
+     */
+    STREFST,
+    /**
+     * STCOAST - Start Coach State
+     */
+    STCOAST,
+    /**
+     * STCONTST - Start Contestant State
+     */
+    STCONTST,
+    /**
+     * STCONTSTR - Start Contestant Strength
+     */
+    STCONTSTR,
+    /**
+     * ADDCONT - Add Contestant
+     */
+    ADDCONT,
+    /**
+     * RMCONT - Remove Contestant
+     */
+    RMCONT,
+    /**
+     * STRP - Set Rope Position
+     */
+    STRP,
+    /**
+     * STG - Start Game
+     */
+    STG,
+    /**
+     * EOG - End Of Game
+     */
+    EOG,
+    /**
+     * EOM - End Of Match
+     */
+    EOM,
 
     // RefereeSite
-    ANG, // Announce New Game
-    INFR, // Inform Referee
-    TRY, // Teams Ready
+    /**
+     * ANG - Announce New Game
+     */
+    ANG,
+    /**
+     * INFR - Inform Referee
+     */
+    INFR,
+    /**
+     * TRY - Teams Ready
+     */
+    TRY,
 
     // Playground
-    STT, // Start Trial
-    WTC, // Wait Trial Conclusion
-    ATD, // Assert Trial Decision
-    DGW, // Declare Game Winner
-    ASTM, // Assemble Team
-    WATL, // Watch Trial
-    FCA, // Follow Coach Advice
-    AD, // Am Done
-    GR, // Get Ready
-    SIP, // Stand In Position
+    /**
+     * STT - Start Trial
+     */
+    STT,
+    /**
+     * WTC - Wait Trial Conclusion
+     */
+    WTC,
+    /**
+     * ATD - Assert Trial Decision
+     */
+    ATD,
+    /**
+     * DGW - Declare Game Winner
+     */
+    DGW,
+    /**
+     * ASTM - Assemble Team
+     */
+    ASTM,
+    /**
+     * WATL - Watch Trial
+     */
+    WATL,
+    /**
+     * FCA - Follow Coach Advice
+     */
+    FCA,
+    /**
+     * AD - Am Done
+     */
+    AD,
+    /**
+     * GR - Get Ready
+     */
+    GR,
+    /**
+     * SIT - Stand In Position
+     */
+    SIP,
 
     // ContestantsBench
-    CLT, // Call Trial
-    DMW, // Declare Match Winner
-    RVN, // Review Notes
-    WFRC, // Wait For Referee Command
-    CLC, // Call Contestants
-    SAB, // Seat At Bench
-    SD, // Seat Down
-    
+    /**
+     * CLT - Call Trial
+     */
+    CLT, 
+    /**
+     * DMW - Declare Match Winner
+     */
+    DMW,
+    /**
+     * RVN - Review Notes
+     */
+    RVN,
+    /**
+     * WFRC - Wait For Referee Command
+     */
+    WFRC,
+    /**
+     * CLC - Call Contestants
+     */
+    CLC,
+    /**
+     * SAB - Seat At Bench
+     */
+    SAB,
+    /**
+     * SD - Seat Down
+     */
+    SD,
+
     // General Purpose
+    /**
+     * SHUT - Shutdown
+     */
     SHUT,
+    /**
+     * SHUTDONE - Shutdown Acknowledgement
+     */
     SHUTDONE,
+    /**
+     * SACK - Set Acknowledgement
+     */
     SACK,
+    /**
+     * ACK - Acknowledgement
+     */
     ACK
 }
