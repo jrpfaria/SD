f9f10c1c6b07d851e71fec79752fda6a5e7c99bf
Rodrigo Faria <jrfaria@ua.pt>
2024-05-02 17:00:26 +0100

    documenting stuff

--- assignment2/commInfra/MessageType.java
+++ assignment2/commInfra/MessageType.java
@@ -1,28 +1,37 @@
 package commInfra;
 
 public enum MessageType { // TODO
+    // GeneralRepos
     SETNFIC,
     NFICDONE,
-    ANG,
-    INFR,
-    TRY,
-    STT,
-    WTC,
-    ATD,
-    DGW,
-    ASTM,
-    WATL,
-    FCA,
-    CLT,
-    DMW,
-    RVN,
-    WFRC,
-    CLC,
-    SAB,
-    SD,
-    AD,
-    GR,
-    SIP,
+
+    // RefereeSite
+    ANG, // Announce New Game
+    INFR, // Inform Referee
+    TRY, // Teams Ready
+    
+    // Playground
+    STT, // Start Trial
+    WTC, // Wait Trial Conclusion
+    ATD, // Assert Trial Decision
+    DGW, // Declare Game Winner
+    ASTM, // Assemble Team
+    WATL, // Watch Trial
+    FCA, // Follow Coach Advice
+    SIP, // Stand In Position
+    GR, // Get Ready
+    AD, // Am Done
+    
+    // ContestantsBench
+    CLT, // Call Trial
+    DMW, // Declare Match Winner
+    RVN, // Review Notes
+    WFRC, // Wait For Referee Command
+    CLC, // Call Contestants
+    SAB, // Seat At Bench
+    SD, // Seat Down
+    
+    // General Purpose
     SHUT,
     SHUTDONE,
     SACK,
