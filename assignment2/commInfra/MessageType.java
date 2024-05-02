c9155cbeae5789e77a50a33823c9ed783f2d1ee6
Rodrigo Faria <jrfaria@ua.pt>
2024-05-02 17:35:04 +0100

    update

--- assignment2/commInfra/MessageType.java
+++ assignment2/commInfra/MessageType.java
@@ -2,38 +2,46 @@ package commInfra;
 
 public enum MessageType { // TODO
     // GeneralRepos
-    SETNFIC,
-    NFICDONE,
-    ANG,
-    INFR,
-    TRY,
-    STT,
-    WTC,
-    ATD,
-    DGW,
-    ASTM,
-    WATL,
-    FCA,
-    CLT,
-    DMW,
-    RVN,
-    WFRC,
-    CLC,
-    SAB,
-    SD,
-    AD,
-    GR,
-    SIP,
-    STREFST,
-    STCOAST,
-    STCONTST,
-    STCONTSTR,
-    ADDCONT,
-    RMCONT,
-    STRP,
-    STG,
-    EOG,
-    EOM,
+    SETNFIC, // Set Log File Name 
+    NFICDONE, // Log File Name Done
+    STREFST, // Start Referee State
+    STCOAST, // Start Coach State
+    STCONTST, // Start Contestant State
+    STCONTSTR, // Start Contestant Strength
+    ADDCONT, // Add Contestant
+    RMCONT, // Remove Contestant
+    STRP, // Set Rope Position
+    STG, // Start Game
+    EOG, // End Of Game
+    EOM, // End Of Match
+
+    // RefereeSite
+    ANG, // Announce New Game
+    INFR, // Inform Referee
+    TRY, // Teams Ready
+
+    // Playground
+    STT, // Start Trial
+    WTC, // Wait Trial Conclusion
+    ATD, // Assert Trial Decision
+    DGW, // Declare Game Winner
+    ASTM, // Assemble Team
+    WATL, // Watch Trial
+    FCA, // Follow Coach Advice
+    AD, // Am Done
+    GR, // Get Ready
+    SIP, // Stand In Position
+
+    // ContestantsBench
+    CLT, // Call Trial
+    DMW, // Declare Match Winner
+    RVN, // Review Notes
+    WFRC, // Wait For Referee Command
+    CLC, // Call Contestants
+    SAB, // Seat At Bench
+    SD, // Seat Down
+    
+    // General Purpose
     SHUT,
     SHUTDONE,
     SACK,
