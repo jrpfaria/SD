6677201130433512a2019ed2468a35994aa91805
danielsilva12UA <104782036+danielsilva12UA@users.noreply.github.com>
2024-05-16 11:54:35 +0100

    Update killeverything.sh

--- assignment3/killeverything.sh
+++ assignment3/killeverything.sh
@@ -1,6 +1,6 @@
 source config.sh
 
-for HOST in l040101-ws01.ua.pt l040101-ws05.ua.pt l040101-ws06.ua.pt l040101-ws07.ua.pt l040101-ws09.ua.pt l040101-ws10.ua.pt
+for HOST in l040101-ws01.ua.pt l040101-ws05.ua.pt l040101-ws06.ua.pt l040101-ws07.ua.pt l040101-ws08.ua.pt l040101-ws09.ua.pt l040101-ws10.ua.pt
 do
     echo $HOST
     for PORT in {22110..22119}
